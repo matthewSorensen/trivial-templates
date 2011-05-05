cf6992f5e8e0c659638307af0adc6cf449d45957
Matthew Sorensen <scholl.ma@gmail.com>
2011-05-05 16:32:33 -0700

    Revert "Made the parser a bit more concise and logical"
    This reverts commit 1c1867ff573e5c65e347a24ddd9189a2e373f7b0.

--- Text/TrivialTemplates/Parser.hs
+++ Text/TrivialTemplates/Parser.hs
@@ -21,7 +21,7 @@ type Template = [Chunk]
 templateParser::Parser Template
 templateParser = template <* endOfInput
                 
-finishTag = skipSpace >> string "}}" >> pure ()
+end = () <$ string "}}"
 space = satisfy isSpace >> skipSpace
 
 literal = B.concat . reverse <$> literal' []
@@ -35,18 +35,31 @@ literal = B.concat . reverse <$> literal' []
           | state < 0  && char == '{'   = Just (0::Int, term)
           | 0 <= state && char == '{'   = Just (state,True)
           | 0 <= state && char == '\\'  = Just (state + 1,term)
-          | otherwise                   = Just (-1, term)          
-tagType::ByteString->Parser ()
-tagType tagType = skipSpace >> string tagType >> finishTag
-
-template = filter validLit <$> temp
-  where temp = (:) . Lit <$> literal <*> (cont <|> return [])
-        cont = (:) <$> (iff <|> var) <*> temp
-        iff  = string "if" >> If <$> (name <* finishTag) <*> template <*> (negative <|> none) <* tagType "end"
-               where negative = tagType "else" >> template
-                     none = pure []
-        var  = Var <$> name <* finishTag
+          | otherwise                   = Just (-1, term)
+var = True
+             
+varOrIf::Parser (Bool,ByteString)
+varOrIf = skipSpace >> (i <|> v) <* skipSpace <* end
+  where i = string "if" >> space >> (not var,) <$> name
+        v = (var,) <$> (name>>= failIfKey)
         name = takeWhile1 isAlpha_iso8859_15
+        failIfKey s
+          | s == "end" || s == "else" = fail ""
+          | otherwise = return s
+tagType::ByteString->Parser ()
+tagType tagType = skipSpace >> string tagType >> skipSpace >> end
+
+template::Parser Template
+template = filter validLit <$> temp 
+  where temp = (:).Lit <$> literal <*> (continue <|> return [])
+        continue = (:) <$> (try varOrIf >>= rest) <*> temp
+        rest (tagType,name)
+          | tagType == var  = return $ Var name
+          | otherwise       = cond name
         validLit (Lit "") = False
         validLit _        = True
-        
+cond::ByteString->Parser Chunk
+cond pro = If pro <$> template <*> (neg <|> none) <* tagType "end"
+  where neg = tagType "else" >> template
+        none = return []
+
