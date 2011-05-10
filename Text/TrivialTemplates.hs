f82ae2e2b2688fcd30c0c74422d1e88d2d54cfb2
Matthew Sorensen <scholl.ma@gmail.com>
2011-05-10 10:46:40 -0700

    Added the start of documentation

--- Text/TrivialTemplates.hs
+++ Text/TrivialTemplates.hs
@@ -1,3 +1,20 @@
+{-| A simple (even /trivial/) template system, offering simple substitution 
+    and conditional inclusion.
+
+A short demonstration of substitution, literals, and conditional inclusion:
+
+>import Data.ByteString.Char8 (pack)
+>import Text.TrivialTemplates
+>import Data.Map (fromList)
+> 
+>env = fromList . map (\(key,val)->(pack key, pack val)) 
+>template = toTemplate "hello, {{object}} {{\\ {{if this}}that{{else}}not that{{end}}"
+>  where toTemplate = either error id . parseTemplate . pack
+>main = do
+>  print $ writeAsByteString template $ env [("object","world"),("this","")]
+>  print $ writeAsByteString template $ env [("object","world")]
+
+-}
 module Text.TrivialTemplates(
   Enviroment,
   Template,  
@@ -16,15 +33,20 @@ import Text.TrivialTemplates.Parser
 
 type Enviroment = Map ByteString ByteString
 
+-- | Parses a template from a 'Data.ByteString', using the template syntax previously outlined. 
+--  If the template fails to parse, a 'Data.Either.Left' value containing an error message.
 parseTemplate::ByteString->Either String Template
 parseTemplate = Atto.eitherResult . parse
 
+-- | Reads and parses a template from a file using 'Data.ByteString.readFile'
 readTemplate::FilePath->IO (Either String Template)
 readTemplate = fmap parseTemplate . readFile
 
 parse::ByteString->Atto.Result Template
 parse = flip Atto.feed empty . Atto.parse templateParser
 
+-- | Renders a template to a 'Blaze.ByteString.Builder.Builder'. If any variables referenced 
+--   in the template aren't present in the 'Environment' an error will be thrown.
 writeAsBuilder::Template->Enviroment->Builder
 writeAsBuilder tmp env = mconcat $ map (write' env) tmp
   where write' _ (Lit a) = fromByteString a
@@ -32,7 +54,8 @@ writeAsBuilder tmp env = mconcat $ map (write' env) tmp
           where f = writeAsBuilder false e
                 t _ = writeAsBuilder true e
         write' e (Var name) = maybe err fromByteString $ lookup name e
-          where err = error $ "Key " ++ show name ++ " not present in enviroment"
+          where err = error $ "Key " ++ show name ++ " not present in environment"
           
+-- | Identical to 'writeAsBuilder', except it renders a template to a 'Data.ByteString'
 writeAsByteString::Template->Enviroment->ByteString
 writeAsByteString = (toByteString .) . writeAsBuilder
